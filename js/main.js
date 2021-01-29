f2d06d91c07f63b47935c56cc85a25cc89a91a6b
Andrey Takchenko <atkachenko1228@gmail.com>
2021-01-29 23:09:24 +0200

    add slider and features sections

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-function a() {
-  console.log(1);
-}
-
-a();
-
-console.log(2);
+$(document).ready(function(){
+  $('.slider__list-big').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipe: false,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider__list-small'
+  });
+  $('.slider__list-small').slick({
+    slidesToShow: 6,
+    infinite: true,
+    asNavFor: '.slider__list-big',
+    dots: true,
+    centerMode: false,
+    focusOnSelect: true
+  });
+});
